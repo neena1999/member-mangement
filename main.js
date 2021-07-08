fec0c49c390bdc124ea06ff59a61b8f27d6e9003
neena1999 <76942357+neena1999@users.noreply.github.com>
2021-07-08 16:48:41 +0700

    js

---
+++ main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#example').DataTable();
+  // ({
+  //   "aLengthMenu": [[5, 10, 25, -1], [5, 10, 25, "All"]],
+  //   "iDisplayLength": 5
+  // });
+  // $('#example').DataTable();
+});
